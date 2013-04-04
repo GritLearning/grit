5a6e7e5d66bc4f0cae907a4cbf63cab4e7b00ddb
Violeng Mam <violeng@web-essentials.asia>
2013-04-04 14:05:13 +0700

    implement quiz

--- assets/www/js/services.js
+++ assets/www/js/services.js
@@ -5,6 +5,10 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('grit.services', []).
-  value('version', '0.1');
-
+angular.module('grit.services', ['ngResource'])
+  .value('version', '0.1')
+  .factory('Quiz', function($resource){
+     return $resource('content/quiz.json', {}, {
+       query: {method:'GET', params:{id:'1'}, isArray:true}
+     });
+  });
