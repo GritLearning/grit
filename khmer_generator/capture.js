4df24bfb2618b5a6942f3d8439ff5168a8c862e4
Breccan <jalnyfen@gmail.com>
2013-07-28 16:04:17 +1200

    Cut down generated image size.

--- khmer_generator/capture.js
+++ khmer_generator/capture.js
@@ -1,4 +1,5 @@
 var page = require('webpage').create();
-page.content = ' ខេមរភាសា '
+page.content = '<html><body> ខេមរភាសា </html></body>';
+page.clipRect = {top: 14, left: 3, width: 70, height: 15};
 page.render('test.png');
 phantom.exit();
